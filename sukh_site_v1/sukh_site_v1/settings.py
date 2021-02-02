13dc82e608b872dc23d5fa006577f08df5bd599e
Sukh <brownie2011@live.ca>
2021-02-02 11:21:01 -0800

    Final build version 1

--- sukh_site_v1/sukh_site_v1/settings.py
+++ sukh_site_v1/sukh_site_v1/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"] 
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'a%15g=js$_rz49z*xskv&!wb(14$mjdk%=fvva913u*hxpq&zm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False) == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'maindjango-zksx6nyxnq-uw.a.run.app', '127.0.0.1', 'sukhmanbhuller.com', 'www.sukhmanbhuller.com']
 
